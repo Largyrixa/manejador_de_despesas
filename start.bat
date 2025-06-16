bd8b24555efa8b18dd59a364a4bac11862ebd1a2
Largyrixa <gartictasemnick@gmail.com>
2025-06-16 19:38:59 -0300

    banco de dados

--- start.bat
+++ start.bat
@@ -1,9 +1,3 @@
 @echo off
 
-docker compose up -d
-
-echo Aguardando o pgAdmin iniciar...
-timeout /t 10
-
-echo Abrindo o pgAdmin no seu navegador...
-start http://localhost:8888
+docker compose up 
